2f5b9fba6f020284ae931a433692bb623ec553fd
Barrikad <s184459@student.dtu.dk>
2022-03-17 20:29:40 +0100

    .

--- tests/frontier_test.py
+++ tests/frontier_test.py
@@ -71,3 +71,30 @@ class TestMockNodes(unittest.TestCase):
         self.frontier.insert(c3)
         ce = self.frontier.extract()
         self.assertEqual(ce,c3)
+
+    def test_rebase(self):
+        p = MockNode()
+        c1 = MockNode()
+        c2 = MockNode()
+        c3 = MockNode()
+        c4 = MockNode()
+        c1.parents = [p]
+        c2.parents = [c1]
+        c3.parents = [p]
+        c4.parents = [c1]
+        p.children = {"a":c1,"c":c3}
+        c1.children = {"b":c2,"d":c4}
+        c1.value = 200
+        c2.value = 200
+        c3.value = 200
+        c4.value = 200
+        c1.steps = 1
+        c2.steps = 2
+        c3.steps = 1
+        c4.steps = 2
+        self.frontier.insert(c2)
+        self.frontier.insert(c3)
+        self.frontier.insert(c4)
+        self.assertEqual(self.frontier.extract(),c3)
+        self.frontier.insert(c3)
+        
